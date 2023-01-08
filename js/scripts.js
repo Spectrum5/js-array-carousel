9a06782b350e807d33f588328bf285b7f8209719
Spectrum5 <toskusergio@gmail.com>
2023-01-09 00:14:14 +0100

    Esercizio completato

---
+++ js/scripts.js
@@ -0,0 +1,44 @@
+//array immagini
+let imagesList = [
+    '../img/01.webp',
+    '../img/02.webp',
+    '../img/03.webp',
+    '../img/04.webp',
+    '../img/05.webp'
+];
+
+let items = document.querySelector('.items');
+let active = 0; 
+    for (let i = 0; i < imagesList.length; i++){
+        items.innerHTML +=
+        `<div class="item"><img src="${imagesList[i]}" alt=""></div>`;             
+}          
+
+document.querySelector('.item').classList.add('d-block');
+let img = document.querySelectorAll('.item');
+
+document.querySelector('.after').addEventListener("click" , function(){
+    console.log(img);
+    img[active].classList.remove('d-block');
+    if(active == img.length-1){
+        active = 0;
+    }
+
+    else{
+        active++;
+        }
+        img[active].classList.add('d-block');
+    });
+
+document.querySelector('.before').addEventListener("click" , function(){
+    
+    console.log(img);
+    img[active].classList.remove('d-block');
+    if(active == 0){   
+        active = img.length-1;
+        }
+        else{
+            active--;
+        }
+        img[active].classList.add('d-block');
+    }); 
