5ecf391a1f645e57b0d671d7110774ff09ea66a8
Spectrum5 <toskusergio@gmail.com>
2023-01-10 15:50:01 +0100

    Modifica immagini array

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,10 @@
 //array immagini
 let imagesList = [
-    '../img/01.webp',
-    '../img/02.webp',
-    '../img/03.webp',
-    '../img/04.webp',
-    '../img/05.webp'
+    'img/01.webp',
+    'img/02.webp',
+    'img/03.webp',
+    'img/04.webp',
+    'img/05.webp'
 ];
 
 let items = document.querySelector('.items');
